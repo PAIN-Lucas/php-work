4fb2ad6e3709c3d46a4517d6dfae29ff5db50182
PAIN-Lucas <lulupain14@gmail.com>
2020-11-18 09:47:13 +0100

    Update functions.php

--- TP3/functions.php
+++ TP3/functions.php
@@ -2,5 +2,20 @@
 function getHeader($startSession,$title){
 	if($startSession){
 		session_start();
-	}
+	}?>
+	<!doctype html>
+	<html>
+	    <head>
+		    <meta charset="utf-8">
+		    <title><?=$title?></title>
+		</head>
+	<body>
+<?php
+}
+
+function getFooter()
+?>
+   </body>
+</html>
+<?php
 }
